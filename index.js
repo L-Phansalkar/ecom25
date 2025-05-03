4658fb6eb19fcae48a59c45da0842d39cca1fbca
L Phansalkar <lphansalkar@Ls-MacBook-Air.local>
2025-05-03 16:47:14 -0400

    first commit

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 const fs = require("fs");
 const pg = require("pg");
 
-const config = {
-  user: "avnadmin",
-  password: "AVNS_gZ6b7UWu0Ho36m-ZyV1",
-  host: "pg-cf3a301-ecom25.k.aivencloud.com",
-  port: "25020",
-  database: "defaultdb",
-  ssl: {
-    rejectUnauthorized: true,
-    ca: fs.readFileSync("./ca.pem").toString(),
-  },
-};
 
 const client = new pg.Client(config);
 client.connect(function (err) {
--- server/index.js
+++ server/index.js
@@ -16,17 +16,6 @@ const db = require('./db')
 const fs = require("fs");
 const pg = require("pg");
 
-const config = {
-  user: "avnadmin",
-  password: "AVNS_gZ6b7UWu0Ho36m-ZyV1",
-  host: "    pg-cf3a301-ecom25.k.aivencloud.com",
-  port: "25020",
-  database: "defaultdb",
-  ssl: {
-    rejectUnauthorized: true,
-    ca: fs.readFileSync("./ca.pem").toString(),
-  },
-};
 
 const client = new pg.Client(config);
 client.connect(function (err) {
