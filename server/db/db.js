43adc798aa49447a96f0c67e0e54463dedefd078
L Phansalkar <lphansalkar@Ls-MacBook-Air.local>
2025-05-03 17:20:08 -0400

    heroku postgres db in sequelize

--- server/db/db.js
+++ server/db/db.js
@@ -5,10 +5,17 @@ const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
 let config
 
-if (process.env.DATABASE_URL) {
+// Use the full database URI for production (e.g., Heroku or RDS)
+const productionDbUrl = 'postgres://ueoael8e5t54f6:p17af9bdfe1675f36a4e3b798b1d102c6ae634353bea93e9a555c163367f4119b@cbec45869p4jbu.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d1gvdriukqfbi0'
+
+const dbUrl = process.env.DATABASE_URL || productionDbUrl || `postgresql://localhost:5432/${databaseName}`
+
+if (dbUrl.includes('amazonaws.com')) {
   config = {
     logging: false,
     ssl: true,
+    dialect: 'postgres',
+    protocol: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
@@ -22,10 +29,7 @@ if (process.env.DATABASE_URL) {
   }
 }
 
-const db = new Sequelize(
-  process.env.DATABASE_URL || `postgresql://localhost:5432/${databaseName}`,
-  config
-)
+const db = new Sequelize(dbUrl, config)
 
 module.exports = db
 
